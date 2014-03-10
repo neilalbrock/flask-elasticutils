02fedd17b09fba8b53936f3ec8b44edaa0314c21
Neil Albrock <neil@atomised.coop>
2014-03-10 12:54:27 +0000

    Get attributes correctly

--- flask_elasticutils.py
+++ flask_elasticutils.py
@@ -21,3 +21,8 @@ class ElasticUtils(object):
             app.extensions = {}
 
         app.extensions['elasticutils'] = S().es(urls=[app.config['ELASTICSEARCH_URL']])
+
+    def __getattr__(self, item):
+        if not 'elasticutils' in current_app.extensions.keys():
+            raise Exception('not initialised, did you forget to call init_app?')
+        return getattr(current_app.extensions['elasticutils'], item)
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def _read(fn):
 
 setup(
     name='Flask-ElasticUtils',
-    version='0.1.5',
+    version='0.1.6',
     url='https://github.com/neilalbrock/flask-elasticutils/',
     license='BSD',
     author='Neil Albrock - Atomised',
